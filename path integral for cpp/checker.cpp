b7e7ace474f956d060e0b24ae25bae43ccbf78c7
charlonia <87217914+charlonia@users.noreply.github.com>
2022-04-03 09:56:24 +0800

    A new version
    Making the result symmetric and consistent with the Matlab.
    It has higher precision and is faster, which may be a good version for academic use.
    The instance used in it is the Fraunhofer diffraction. You can easily transform it into two-slit interference. And further, the Aharonove-Bohm effect, which is the goal of the research

--- path integral for cpp/checker.cpp
+++ path integral for cpp/checker.cpp
@@ -1,4 +1,8 @@
 //
+// Created by charlonia on 2022/4/2.
+//
+
+//
 // Created by charlonia on 2022/3/26.
 //
 //
@@ -18,7 +22,7 @@ int main(){
     double grid =1;
     long double grid_div=0.01;
     double time_div = 0.002; //时间间隔
-    double time_all = 0.003;     //总的演化时间
+    double time_all = 0.001;     //总的演化时间
     long double d = 0.4/grid_div;          //狭缝宽度
     double v=10; // 设置电子入射速度a
     double hb=1;
@@ -46,9 +50,9 @@ int main(){
     const int totaltime = round(time_all/time_div);
     int low;
     int high;
-    long double lo;
-    long double hi;
     int midx = round(totallen/2)-1;
+    long double hi;
+    long double lo;
     complex<long double> A[totallen][totallen];
     complex<long double> B[totallen][totallen];
     complex<long double> C[totallen][totallen];
@@ -73,66 +77,31 @@ int main(){
     for (int j=0;j<totaltime;++j){
         cout <<j<<endl;
         clock_t start_time=clock();
-        for (int kx=0;kx < round(totallen/2)-1;++kx) {
-            for (int ky = 0; ky < totallen; ++ky) {
-                for (int lx = 0; lx < round(totallen / 2) - 1; ++lx) {
-                    for (int ly = 0; ly < totallen; ++ly) {
-                        B[ky][kx] +=A[ly][lx] * C[abs(ly-ky)][abs(lx-kx)];
-                    }
-                }
-                for (int lx = round(totallen / 2)-1; lx < totallen - 1; ++lx) {
-                    lo = ((totallen - d) / 2 - ky) / (totallen / 2 - kx-1) * (lx - kx) + ky+1;
-                    low = floor(lo);
-                    if (abs( round(lo)- (lo))<1/(long double)totallen){
-                        low = round(lo);
-                    }
-                    low = max(low, 1);
-                    hi = ((totallen + d) / 2 - ky-1) / (totallen / 2 - kx-1) * (lx - kx) + ky+1;
-                    high = ceil(hi);
-                    if (abs(round(hi)-(hi))<1/(long double)totallen){
-                        high = round(hi);
-                    }
-                    high = min(high, totallen);
-                    for (int ly = low-1; ly < high; ++ly) {
-                        B[ky][kx] +=A[ly][lx] * C[abs(ly-ky)][abs(lx-kx)];
-                    }
-                }
-            }
-        }
-        for (int ky=round((totallen - d) / 2);ky<round((totallen + d) / 2);++ky){
-            for(int lx = 0;lx < totallen - 1;++lx){
-                for(int ly = 0;ly < totallen ;++ly){
-                    B[ky][midx] +=A[ly][lx] * C[abs(ly-ky)][abs(lx-midx)];
+        int ky =23;
+        int kx =70;
 
-                }
+        for (int lx = round(totallen / 2)-1; lx < totallen - 1; ++lx) {
+            lo = ((totallen - d) / 2 - ky) / (totallen / 2 - kx-1) * (lx - kx) + ky+1;
+            low = floor(lo);
+            if (abs( round(lo)- (lo))<1/(long double)totallen){
+                low = round(lo);
+                cout<<"启动"<<endl;
             }
-        }
-        for (int kx=round(totallen/2);kx < totallen;++kx){
-            for(int ky=0;ky < totallen;++ky){
-                for (int lx = round(totallen/2); lx < totallen - 1; ++lx) {
-                    for (int ly = 0; ly < totallen; ++ly) {
-                        B[ky][kx] +=A[ly][lx] * C[abs(ly-ky)][abs(lx-kx)];
-                    }
-                }
-                for (int lx = 0; lx <round(totallen / 2); ++lx) {
-                    lo = ((totallen - d) / 2 - ky) / (totallen / 2 - kx-1) * (lx - kx) + ky+1;
-                    low = floor(lo);
-                    if (abs( round(lo)- (lo))<1/(long double)totallen){
-                        low = round(lo);
-                    }
-                    low = max(low, 1);
-                    hi = ((totallen + d) / 2 - ky-1) / (totallen / 2 - kx-1) * (lx - kx) + ky+1;
-                    high = ceil(hi);
-                    if (abs(round(hi)-(hi))<1/(long double)totallen){
-                        high = round(hi);
-                    }
-                    high = min(high, totallen);
-                    for (int ly = low-1; ly < high; ++ly) {
-                        B[ky][kx] +=A[ly][lx] * C[abs(ly-ky)][abs(lx-kx)];
-                    }
-                }
+            low = max(low, 1);
+            hi = ((totallen + d) / 2 - ky-1) / (totallen / 2 - kx-1) * (lx - kx) + ky+1;
+            high = ceil(hi);
+            if (abs(round(hi)-(hi))<1/(long double)totallen){
+                high = round(hi);
             }
-        }
+            high = min(high, totallen);
+            for (int ly = low-1; ly < high; ++ly) {
+                        B[ly][lx] +=(complex<long double>){1.1111111,1.111111111111111};
+                    }
+                }
+        int lx = 67;
+        lo = ((totallen - d) / 2 - ky) / (totallen / 2 - kx-1) * (lx - kx) + ky+1;
+        cout<<"结果"<<((round(lo)-lo)<1/(long double)totallen)<<endl;
+        cout<<"结果"<<(1/(long double)totallen)<<endl;
         for (int y =0;y<totallen;++y){
             for(int x = 0; x<totallen;++x){
                 A[y][x]=B[y][x];
@@ -150,7 +119,7 @@ int main(){
     cout <<"总"<< "The run time is: " <<(double)(end_time - start_time) / CLOCKS_PER_SEC <<endl;
     ofstream opt;
     opt.precision(20);
-    opt.open("output.csv");
+    opt.open("checker.csv");
     for(int j=0;j<totallen;j++) {
         for (int i = 0; i <totallen; i++) {
             opt << real(A[j][i]);
@@ -163,5 +132,4 @@ int main(){
         }
         opt<<endl;
     }
-
 }
